f4f4bb7fa5217c62694ab7fd5eac3d5382afb5fa
Jonathan Benz <98258254+JonathanBenz@users.noreply.github.com>
2025-04-06 23:40:22 -0400

    WebGL Final Build
    -Also, caught a last minute bug in Microtransactions.cs 😃

--- Assets/Scripts/Monetization/Microtransactions.cs
+++ Assets/Scripts/Monetization/Microtransactions.cs
@@ -4,73 +4,32 @@ using UnityEngine;
 
 public class Microtransactions : MonoBehaviour
 {
-    [SerializeField] GameObject transactionScreen;
-    [SerializeField] GameObject transactionButton;
     [SerializeField] GameObject AdRemoveButton;
     [SerializeField] GameObject AmericaButton;
-
-    bool isDisablingAds;
-    bool isBuyingAmerica;
-    bool isBuyingPremium;
 
     private void OnEnable()
     {
         Time.timeScale = 0;
     }
-   
-    public void CheckIfCanPurchase(int amount)
-    {
-        int currentPremium = FindObjectOfType<Shop>().premiumMoney;
-
-        if (currentPremium >= amount) 
-        { 
-            transactionScreen.SetActive(true); 
-            transactionButton.SetActive(true); 
-        }
-        else
-        {
-            isDisablingAds = false;
-            isBuyingAmerica = false;
-            return;
-        }
-    }
-
-    public void IsDisablingAds(bool bValue)
-    {
-        isDisablingAds = bValue;
-    }
-    public void IsBuyingAmerica(bool bValue)
-    {
-        isBuyingAmerica = bValue;
-    }
-    public void IsBuyingPremium(bool bValue)
-    {
-        isBuyingPremium = bValue;
-    }
 
     public void DisableAds()
     {
-        if (isDisablingAds)
+        int currentPremium = FindObjectOfType<Shop>().premiumMoney;
+        if (currentPremium >= 2)
         {
             FindObjectOfType<Advertisements>().gameObject.SetActive(false);
             FindObjectOfType<Shop>().SpendPremiumCurrency(2);
             Destroy(AdRemoveButton);
-            isDisablingAds = false;
         }
     }
     public void BuyAmerica()
     {
-        if (isBuyingAmerica)
+        int currentPremium = FindObjectOfType<Shop>().premiumMoney;
+        if (currentPremium >= 7)
         {
             FindObjectOfType<Shop>().SpendPremiumCurrency(7);
             Destroy(AmericaButton);
-            isBuyingAmerica = false;
         }
-    }
-
-    public void BuyPremium(int amount)
-    {
-        if (isBuyingPremium) FindObjectOfType<Shop>().AddPremiumCurrency(amount);
     }
 
     public void ResumeTime()
